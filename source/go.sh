b1cff6aacf63744bb091aff03b3e0ef8001780cb
Paul Robson <paul@robsons.org.uk>
2017-10-10 19:50:05 +0100

    JS tweaks

--- source/go.sh
+++ source/go.sh
@@ -1,6 +1,7 @@
 cd ../emulator
 make
 cd ../source
-python ../assembler/copasm.py variables.asm main.asm system.asm && tail -n 20 rom.lst && ../emulator/cosmos 5
+python ../assembler/copasm.py variables.asm main.asm system.asm && tail -n 20 rom.lst && 
 cp rom.ts ../javascript/src/generated
 cp holograms/* ../javascript/assets/holograms
+../emulator/cosmos 5
