083e0cd18f2f79301145ea9e6d5bcfe8dd49e113
Paul Robson <paul@robsons.org.uk>
2017-10-16 23:11:16 +0100

    tweak

--- source/go.sh
+++ source/go.sh
@@ -3,5 +3,4 @@ make
 cd ../source
 python ../assembler/copasm.py  && tail -n 35 rom.lst && 
 cp rom.ts ../javascript/src/generated
-cp holograms/* ../javascript/assets/holograms
 ../emulator/cosmos 0
