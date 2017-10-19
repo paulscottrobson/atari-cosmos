6c81fa85d14c19dc1d75d919d3eeee3eb08957ec
Paul Robson <paul@robsons.org.uk>
2017-10-19 22:41:55 +0100

    Chaotic first version

--- source/games/superman.asm
+++ source/games/superman.asm
@@ -7,14 +7,14 @@
 ; **********************************************************************************************************
 ;
 ;		dragon.0 dragon.1 are the positions on screen (dragon0.0 == offscreen)
-;  		dragon.2 bit 0 is left/right
+;  		dragon.2 bit 0 is left (1)/right (0)
 ; 		dragon.2 bit 1 is up moving.
 ;		dragon.2 bit 2 set if in jail
 ;
 ; 		2,12 # of dragon being carried (or $F if none)
 ; 		2,11 counter timer
 
-SUDragonCount = 9 							; # of dragons in game.
+SUDragonCount = 6 							; # of dragons in game.
 
 	page
 
@@ -23,6 +23,8 @@ Superman:
 	stii 	15 								; not carrying a dragon.
 	stii 	9								; this game we count down, it's as fast as possible.
 	stii 	9
+	lbi 	1,Player 						; reposition player
+	stii 	8+1
 ;
 ;	Main game loop
 ;
@@ -35,7 +37,7 @@ SULoop:
 ;
 ;	Check if we have won - are the cells all full.
 ;
-	lbi 	0,SUDragonCount 				; count the number of dragons in jail
+	lbi 	2,SUDragonCount 				; count the number of dragons in jail
 	clra 	
 SUCountJailed:
 	skmbz 	2 								; skip if dragon in jail bit clear
@@ -65,14 +67,24 @@ SUCountJailed:
 
 SUDragonCode:
 
-	; TODO:check if alive, create if not, perhaps.
-	; TODO:if alive, check escape if in dungeon, move up.
-	; TODO:if not in dungeon, move it.
+	lbi 	0,SUDragonCount 				; work through the dragons
+SUDragonLoop:
 
+	; TODO:if alive, check escape if in dungeon, move up.
+
+	jsrp 	CheckPixelInUse 				; skip if not in use
+	jsr 	SUMoveDragon
+	jsrp 	CheckPixelInUse 				; skip if not in use
+	jp 		SUNoCreate
+	jsr 	SUCreateDragon 					; possibly create a dragon
+SUNoCreate:
 
 	; TODO:dragon caught by player ?
 	; TODO:dragon dropped in empty cell by player ?
 
+	ld 		0 								; do next dragon
+	xds 	0
+	jp 		SUDragonLoop
 	jmp 	SULoop
 
 ;
@@ -93,4 +105,67 @@ SUGameOver:
 	stii 	0 								; zero the score
 	stii 	0
 	jmp 	ShowHolo2LifeLost 				; this game termination here.
-	
+;
+;	Possibly create dragon at 0,B
+;
+SUCreateDragon:	
+	jsrp 	Random 							; random#
+	aisc  	4 								; creates 0,1,2,3 and skip
+	ret
+	aisc 	1+8 							; fix up the Y value	
+
+	x 		0 								; save in X
+	clra
+	aisc 	1
+	x 		1 								; set X to 1, restore and switch to Y
+	x 		3 								; save in Y and switch to 2
+	clra 									; set this to zero
+	x  		0
+
+	jsrp 	Random 							; chance of moving up
+	aisc 	11 								; mostly skip
+	smb 	1 								; set the up moving bit.
+	ld 		2 								; switch back to zero page (X)
+
+	jsrp 	Random 							; 50/50 chance we do the right side
+	aisc 	8
+	ret
+	smb 	2 								; set X = 7
+	smb 	1
+	ld 		2 								; switch to page 2
+	smb 	0 								; direction is left.
+	ld 		2 								; back to page 0
+	ret
+;
+;	Move dragon at 0,B
+;
+SUMoveDragon:
+	ld 		2								; point to flag page 2
+	skmbz 	0 								; if zero move right
+	jp 		SUMDLeft
+
+	ld 		2 								; move right code
+	jsrp 	MoveRight
+	jp 		__SUMDMoveUpQ
+
+__SUMDKillExit:
+	jmp 	Kill
+
+SUMDLeft:
+	ld 		2 								; move left code
+	jsrp 	MoveLeft
+	jp 		__SUMDMoveUpQ
+	jp 		__SUMDKillExit
+
+__SUMDMoveUpQ:
+	ld 		2 								; to page 2
+	skmbz 	1 								; if up bit is clear
+	jp 		__SUMDMoveUp
+	ld 		2
+	ret
+
+__SUMDMoveUp:
+	ld 		2 								; back to page 0
+	jsrp 	MoveUp
+	ret
+	jp 		__SUMDKillExit
